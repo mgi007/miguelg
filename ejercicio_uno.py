8d3d6b6e5d7e5ee1eabaf78145ef4b029c79dcde
Manuel Veiga <Manuel Veiga>
2019-11-26 20:06:59 +0100

    comit para colision

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -1,8 +1,13 @@
 #FUNCIONA
+
 """
 Este programa imprime en pantalla los múltiplos de 5 menores que 25
 """
 
 for i in range(1, 25, 1):
     if i % 5 == 0:
+        print(i)´
+
+for i in range(1, 25, 1):
         print(i)
+#midificado
